a74720514e0c7a37322d01f35e85b042e97a240f
stocke777 <deshwaljaivardhan@gmail.com>
2022-04-19 17:25:16 +0530

    final build

--- index.js
+++ index.js
@@ -187,7 +187,7 @@ class Fighter extends Sprite {
             this.animateFrames()
         }
         
-        c.fillRect(this.attackBox.position.x, this.attackBox.position.y, this.attackBox.width, this.attackBox.height)
+        // c.fillRect(this.attackBox.position.x, this.attackBox.position.y, this.attackBox.width, this.attackBox.height)
 
         this.position.x += this.velocity.x
         this.position.y += this.velocity.y
